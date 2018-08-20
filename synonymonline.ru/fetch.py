509a9e1610b349e857ee25c594613382dd0cb44b
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-08-20 21:14:39 +0700

    input format

--- synonymonline.ru/fetch.py
+++ synonymonline.ru/fetch.py
@@ -1,5 +1,6 @@
 ﻿#!/usr/bin/python3
 
+import sys
 import json
 from urllib.request import Request, urlopen
 import openpyxl
@@ -8,20 +9,31 @@ import xlsxwriter
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0"}
 
-ifn = "in.xlsx"
-iwb = openpyxl.load_workbook(ifn)
-iws = iwb[iwb.get_sheet_names()[0]]
+
+if len(sys.argv) < 3:
+	print("usage: python.exe ./fetch.py ((<infile.xlsx>) | (<word0> [word1] [word2] ... [wordN])) <outfile.xlsx>")
 
 words = []
+if sys.argv[1].endswith(".xlsx"):
+	ifn = sys.argv[1]
+	iwb = openpyxl.load_workbook(ifn)
+	iws = iwb[iwb.get_sheet_names()[0]]
 
-k = 0
-while True:
-	word = iws.cell(row=(1 + k), column=1).value
-	if word is None:
-		break;
-	word = str(word).strip()
-	words.append(word)
-	k += 1
+	k = 0
+	while True:
+		word = iws.cell(row=(1 + k), column=1).value
+		if word is None:
+			break;
+		word = str(word).strip()
+		words.append(word)
+		k += 1
+else:
+	for w in sys.argv[1:-1]:
+		words.append(w)
+
+ofn = sys.argv[-1]
+owb = xlsxwriter.Workbook(ofn)
+ows = owb.add_worksheet()
 
 synonyms = []
 encoding = "utf-8"
@@ -35,9 +47,6 @@ for word in words:
 	res = urlopen(req).read().decode(encoding)
 	synonyms.append(json.loads(res))
 
-ofn = "out.xlsx"
-owb = xlsxwriter.Workbook(ofn)
-ows = owb.add_worksheet()
 for i, res in enumerate(synonyms):
 	ows.write(i, 0, res["word"])
 	ows.write(i, 1, res["word_baseform"])
